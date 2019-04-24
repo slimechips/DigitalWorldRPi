09a81c59bc90b9ee92c3818f7906541dfb3aca6e
storyinvisible <storyinvisible@gmail.com>
2019-04-24 14:14:33 +0800

    trendline hander with comment

--- trendline_handler.py
+++ trendline_handler.py
@@ -3,30 +3,32 @@ import datetime
  
 
 
-url = "https://digitalworldf08g2.firebaseio.com/"
-apikey = "AIzaSyDHyug6TDWAda_ZirZ1G7B9cFV525ahvyk"
+# url = "https://digitalworldf08g2.firebaseio.com/"
+# apikey = "AIzaSyDHyug6TDWAda_ZirZ1G7B9cFV525ahvyk"
 
-config = {
-    "apiKey": apikey,
-    "databaseURL": url,
-}
+# config = {
+#     "apiKey": apikey,
+#     "databaseURL": url,
+# }
 
-firebase = pyrebase.initialize_app(config)
-db = firebase.database()
+# firebase = pyrebase.initialize_app(config)
+# db = firebase.database()
 
 class trend_handler:
 	def __init__(self, db,store):
+		#the object takes in two parameters, the name of the store and a database object 
 		self.db=db
 		self.store=store
 	def get_popular(self):
+		#The function is to get the sales of each item in the store
 		all_items={} #
 		complete=self.db.child('completed_orders').child('western_stall').get()
 		for entry in complete.each():
 			items = entry.val()['food_item']
 			if items in all_items:
-				all_items[items]+=1
+				all_items[items]+=1 # if the item exist in the dictionary 
 			else:
-				all_items[items]=1
+				all_items[items]=1# if the item does not exist, it means that the 
 		return all_items
 	def get_sales (self):
 		sale=[]
@@ -43,9 +45,9 @@ class trend_handler:
 			sale.append([[date_time_obj.year,date_time_obj.month,date_time_obj.day],float(price.val()[1:])])
 		return sale# return 2d array 
 
-trend=trend_handler(db,'western_stall')
-sales=trend.get_sales()
-print(sales)
-popular=trend.get_popular()
-print("printing trend")
-print(popular)
+# trend=trend_handler(db,'western_stall')
+# sales=trend.get_sales()
+# print(sales)
+# popular=trend.get_popular()
+# print("printing trend")
+# print(popular)
